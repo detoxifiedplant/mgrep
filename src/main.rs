5d13f0d301845f685fbb8d479f44eccb9e4a57d5
kamal <detoxifiedplant@gmail.com>
2024-06-19 18:17:03 +0530

    closure commit

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,20 @@
+#![allow(unused_variables)]
+#![allow(unused_imports)]
+#![allow(dead_code)]
 use std::env;
-use std::fs;
+use std::process;
+use mgrip::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let path = &args[2];
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
 
-    println!("query is: {}", query);
-    println!("path is: {}", path);
-
-    let contents = fs::read_to_string(path)
-        .expect("not able to read");
-
-    println!("content is: \n{contents}")
+    if let Err(err) = mgrip::run(config){
+        eprintln!("Application Error: {}", err);
+        process::exit(1);
+    }
 }
